df75ea2b76b18a3ca6e8b4ae75273f2d1c86ba21
ChrisJohDev <chris@chrisjohannesson.com>
2021-05-18 22:36:36 +0200

    About, Contact and Privacy pages done with styling.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,21 @@ namespace layout_views.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
